79e871dc3ae9f13b9cc5ccb1ffae8b9a7ebb80b3
SamaelTessier <samael.tessier@gmail.com>
2021-12-16 13:29:06 -0500

    new patches

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -21,6 +21,103 @@ namespace RestfulApi.Controllers
             _context = context;
         }
 
+        [HttpPatch ("HeadquarterUpdate/{id}&{company_headquarter}")]
+        public string HeadquarterUpdate (long id,string company_headquarter)
+        {
+            var InfoCustomer = _context.customers.Find(id);
+            if (InfoCustomer == null) {
+                return "Please use a valid customer id" ;
+            }
+            else {
+                    InfoCustomer.company_headquarter = $"{company_headquarter}";
+ 
+                _context.customers.Update (InfoCustomer);
+                _context.SaveChanges ();
+                return "The company headquarter has been successufully changed to "+$"{company_headquarter}";
+            }   
+        }
+
+        [HttpPatch ("ContactUpdate/{id}&{company_contact}")]
+        public string ContactUpdate (long id,string company_contact)
+        {
+            var InfoCustomer = _context.customers.Find(id);
+            if (InfoCustomer == null) {
+                return "Please use a valid customer id" ;
+            }
+            else {
+                    InfoCustomer.company_contact = $"{company_contact}";
+ 
+                _context.customers.Update (InfoCustomer);
+                _context.SaveChanges ();
+                return "The company contact has been successufully changed to "+$"{company_contact}";
+            }   
+        }
+
+        [HttpPatch ("DescriptionUpdate/{id}&{company_description}")]
+        public string DescriptionUpdate (long id,string company_description)
+        {
+            var InfoCustomer = _context.customers.Find(id);
+            if (InfoCustomer == null) {
+                return "Please use a valid customer id" ;
+            }
+            else {
+                    InfoCustomer.company_description = $"{company_description}";
+ 
+                _context.customers.Update (InfoCustomer);
+                _context.SaveChanges ();
+                return "The company description has been successufully changed to "+$"{company_description}";
+            }   
+        }
+
+        [HttpPatch ("TechNameUpdate/{id}&{service_technical_authority_name}")]
+        public string TechNameUpdate (long id,string service_technical_authority_name)
+        {
+            var InfoCustomer = _context.customers.Find(id);
+            if (InfoCustomer == null) {
+                return "Please use a valid customer id" ;
+            }
+            else {
+                    InfoCustomer.service_technical_authority_name = $"{service_technical_authority_name}";
+ 
+                _context.customers.Update (InfoCustomer);
+                _context.SaveChanges ();
+                return "The company's technical authority name has been successufully changed to "+$"{service_technical_authority_name}";
+            }   
+        }
+
+        [HttpPatch ("TechPhoneUpdate/{id}&{technical_authority_phone}")]
+        public string TechPhoneUpdate (long id,string technical_authority_phone)
+        {
+            var InfoCustomer = _context.customers.Find(id);
+            if (InfoCustomer == null) {
+                return "Please use a valid customer id" ;
+            }
+            else {
+                    InfoCustomer.technical_authority_phone = $"{technical_authority_phone}";
+ 
+                _context.customers.Update (InfoCustomer);
+                _context.SaveChanges ();
+                return "The company's technical authority phone # has been successufully changed to "+$"{technical_authority_phone}";
+            }   
+        }
+
+        [HttpPatch ("TechEmailUpdate/{id}&{service_technical_authority_email}")]
+        public string TechEmailUpdate (long id,string service_technical_authority_email)
+        {
+            var InfoCustomer = _context.customers.Find(id);
+            if (InfoCustomer == null) {
+                return "Please use a valid customer id" ;
+            }
+            else {
+                    InfoCustomer.service_technical_authority_email = $"{service_technical_authority_email}";
+ 
+                _context.customers.Update (InfoCustomer);
+                _context.SaveChanges ();
+                return "The company's technical authority email has been successufully changed to "+$"{service_technical_authority_email}";
+            }   
+        }
+
+
 
         // GET: api/Customers/{email}
         [HttpGet("{company_email}")]
