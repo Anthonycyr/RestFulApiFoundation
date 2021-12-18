c4ccb901f75d2b8e1c51a0204de110e9a08ef9df
SamaelTessier <samael.tessier@gmail.com>
2021-12-17 21:22:17 -0500

    elevator

--- Models/Elevator.cs
+++ Models/Elevator.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
-
+using System;
 namespace RestfulApi.Models
 {
     public class Elevator
     {
         public long Id { get; set; }
+        public int serial_number { get; set; }
+        public string model { get; set; }
         public string status { get; set; }
+        public DateTime date_of_last_inspection { get; set; }
+        public DateTime created_at { get; set; }
         public int column_id { get; set; }
     }
 }
