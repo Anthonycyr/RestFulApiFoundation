9d1b123d5f023720a42f515cb8deffb9f969ff2e
SamaelTessier <samael.tessier@gmail.com>
2021-12-17 20:38:18 -0500

    column

--- Models/Column.cs
+++ Models/Column.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 
 namespace RestfulApi.Models
 {
@@ -6,6 +7,8 @@ namespace RestfulApi.Models
     {
         public long Id { get; set; }
         public string status { get; set; }
+        public int number_of_floors_served { get; set; }
+        public DateTime created_at { get; set; }
         public int battery_id { get; set; }
     }
 }
